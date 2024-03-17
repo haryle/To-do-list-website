4f6f72b2a7f8f4da1513251d408de08b8ac5a6e3
harryle <lehoangsonsg@gmail.com>
2024-03-17 23:07:27 +1030

    feat: add test add project #3

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,21 +2,19 @@ import pytest
 from advanced_alchemy.extensions.litestar import SQLAlchemyPlugin
 from litestar import Litestar
 from litestar.testing import AsyncTestClient
-
+from pathlib import Path
 from backend.helpers import create_db_config, provide_transaction
 from backend.router import ProjectController, TagController, TaskController
 
-db_config = create_db_config("test.sqlite")
 
 @pytest.fixture(scope="function")
-def app() -> Litestar:
-    return Litestar(
+def test_client():
+    p = Path("test.sqlite")
+    db_config = create_db_config(p)
+    app = Litestar(
         [ProjectController, TaskController, TagController],
         dependencies={"transaction": provide_transaction},
-        plugins=[SQLAlchemyPlugin(db_config)]
+        plugins=[SQLAlchemyPlugin(db_config)],
     )
-
-
-@pytest.fixture(scope="function")
-async def test_client() -> AsyncTestClient:
-    return AsyncTestClient(app=app)
+    yield AsyncTestClient(app)
+    p.unlink()
--- tests/test_project.py
+++ tests/test_project.py
@@ -3,21 +3,24 @@ from litestar.testing import AsyncTestClient
 
 from backend.model import Project
 
-pytest_plugins = ('pytest_asyncio',)
+pytest_plugins = ("pytest_asyncio",)
 
 
 @pytest.mark.asyncio
-async def test_add_project(app) -> None:
-    project = Project(title="first_project")
-    async with AsyncTestClient(app) as client:
-        result = await client.post(
-            "/project", json=project.to_dict()
+async def test_add_project_to_db(test_client) -> None:
+    async with test_client as client:
+        first_project = Project(title="first_project")
+        await client.post("/project", json=first_project.to_dict())
+
+        fetch_result = await client.get(
+            "/project", params={"title": first_project.title}
         )
-        assert result.status_code == 201
-        assert result.json()['title'] == project.title
-        get_result = await client.get("/project", params={"title": project.title})
-        assert get_result.status_code == 200
-        assert len(get_result.json()) == 1
-        item_id = get_result.json()[0].id
+        assert fetch_result.status_code == 200
+        assert len(fetch_result.json()) == 1
+        item_id = fetch_result.json()[0]["id"]
         get_result_by_id = await client.get(f"/project/{item_id}")
-        assert get_result_by_id.json().title == project.title
+        assert get_result_by_id.json()["title"] == first_project.title
+        delete_result = await client.delete(f"/project/{item_id}")
+        assert delete_result.status_code == 204
+        get_result_by_id = await client.get(f"/project/{item_id}")
+        assert get_result_by_id.status_code == 404
