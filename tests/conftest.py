fe4829547877dbd9928dd92c37ef655a0c56296b
hoangson <lehoangsonsg@gmail.com>
2024-03-18 01:01:23 +1030

    feat: add test cases for Project #3

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,5 @@
+from pathlib import Path
+
 import pytest
 from advanced_alchemy.extensions.litestar import SQLAlchemyPlugin
 from litestar import Litestar
@@ -6,17 +8,15 @@ from litestar.testing import AsyncTestClient
 from backend.helpers import create_db_config, provide_transaction
 from backend.router import ProjectController, TagController, TaskController
 
-db_config = create_db_config("test.sqlite")
 
 @pytest.fixture(scope="function")
-def app() -> Litestar:
-    return Litestar(
+def test_client() -> AsyncTestClient:
+    p = Path("test.sqlite")
+    db_config = create_db_config("test.sqlite")
+    app = Litestar(
         [ProjectController, TaskController, TagController],
         dependencies={"transaction": provide_transaction},
         plugins=[SQLAlchemyPlugin(db_config)]
     )
-
-
-@pytest.fixture(scope="function")
-async def test_client() -> AsyncTestClient:
-    return AsyncTestClient(app=app)
+    yield AsyncTestClient(app=app)
+    p.unlink(missing_ok=True)
--- tests/test_project.py
+++ tests/test_project.py
@@ -1,23 +1,87 @@
 import pytest
-from litestar.testing import AsyncTestClient
+from httpx import Response
 
 from backend.model import Project
 
-pytest_plugins = ('pytest_asyncio',)
 
+class TestProject:
+    title: str = "test_project"
 
-@pytest.mark.asyncio
-async def test_add_project(app) -> None:
-    project = Project(title="first_project")
-    async with AsyncTestClient(app) as client:
-        result = await client.post(
-            "/project", json=project.to_dict()
-        )
-        assert result.status_code == 201
-        assert result.json()['title'] == project.title
-        get_result = await client.get("/project", params={"title": project.title})
-        assert get_result.status_code == 200
-        assert len(get_result.json()) == 1
-        item_id = get_result.json()[0].id
-        get_result_by_id = await client.get(f"/project/{item_id}")
-        assert get_result_by_id.json().title == project.title
+    @pytest.fixture(scope="function")
+    async def project_response(self, test_client) -> Response:
+        project = Project(title=self.title)
+        async with test_client as client:
+            response = await client.post(
+                "/project", json=project.to_dict()
+            )
+        yield response
+        async with test_client as client:
+            await client.delete(f"/project/{project.id}")
+
+    async def test_response_is_successful(self, project_response):
+        assert project_response.status_code == 201
+        assert project_response.json()["title"] == self.title
+        assert project_response.json()["id"] is not None
+
+    async def test_create_another_project_successful(
+            self, project_response, test_client
+            ):
+        project = Project(title=self.title + "_1")
+        async with test_client as client:
+            response = await client.post(
+                "/project", json=project.to_dict()
+            )
+            assert response.status_code == 201
+            assert response.json()["title"] == self.title + "_1"
+            project_id = response.json()["id"]
+            assert response.json()["id"] is not None
+            response = await client.get("/project")
+            assert response.status_code == 200
+            assert len(response.json()) == 2
+            await client.delete(f"/project/{project_id}")
+            response = await client.get("/project")
+            assert response.status_code == 200
+            assert len(response.json()) == 1
+
+    async def test_find_by_id_return_the_same_obj(self, project_response, test_client):
+        project_id = project_response.json()["id"]
+        async with test_client as client:
+            result = await client.get(f"/project/{project_id}")
+            assert result.status_code == 200
+            assert result.json()["id"] == project_id
+            assert result.json()["title"] == self.title
+
+    async def test_find_by_title_return_the_same_obj(
+            self, project_response, test_client
+    ):
+        project_id = project_response.json()["id"]
+        async with test_client as client:
+            result = await client.get(f"/project/", params={"title": self.title})
+            assert result.status_code == 200
+            assert len(result.json()) == 1
+            assert result.json()[0]["id"] == project_id
+            assert result.json()[0]["title"] == self.title
+
+    async def test_find_wrong_title_return_nothing(self, project_response, test_client):
+        async with test_client as client:
+            result = await client.get(f"/project/", params={"title": self.title + "_"})
+            assert result.status_code == 200
+            assert len(result.json()) == 0
+
+    async def test_create_same_title_integrity_error(
+            self, project_response, test_client
+    ):
+        project = Project(title=self.title)
+        async with test_client as client:
+            response = await client.post(
+                "/project", json=project.to_dict()
+            )
+            assert response.status_code == 409
+
+    async def test_delete_object_no_longer_exist(self, project_response, test_client):
+        project_id = project_response.json()["id"]
+        async with test_client as client:
+            response = await client.delete(f"/project/{project_id}")
+            assert response.status_code == 204
+            result = await client.get(f"/project/{project_id}")
+            assert result.status_code == 404
