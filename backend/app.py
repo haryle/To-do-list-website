eee2adb34df131cd798891a2e28a5c41bd4c1ff1
harryle <lehoangsonsg@gmail.com>
2024-03-21 09:41:29 +1030

    feat: add cors middleware for frontend fetching

--- backend/app.py
+++ backend/app.py
@@ -1,17 +1,17 @@
 from advanced_alchemy.extensions.litestar import SQLAlchemyPlugin
 from litestar import Litestar
+from litestar.config.cors import CORSConfig
 
 from backend.helpers import create_db_config, provide_transaction
 from backend.router import ProjectController, TagController, TaskController
 
 db_config = create_db_config("todo.sqlite")
+cors_config = CORSConfig(allow_origins=["*"])
+
 
 app = Litestar(
-    [
-        ProjectController,
-        TaskController,
-        TagController
-    ],
+    [ProjectController, TaskController, TagController],
     dependencies={"transaction": provide_transaction},
     plugins=[SQLAlchemyPlugin(db_config)],
+    cors_config=cors_config,
 )
