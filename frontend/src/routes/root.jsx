e7a9529d3e2ec57d522c6a559e1ddfc0301b26a6
harryle <lehoangsonsg@gmail.com>
2024-03-21 11:17:23 +1030

    feat: add side bar create button

--- frontend/src/routes/root.jsx
+++ frontend/src/routes/root.jsx
@@ -1,85 +1,12 @@
 import { Outlet } from "react-router-dom";
-import { NavLink, Form, useLoaderData } from "react-router-dom";
-import { getProjects, getTags } from "../data";
-
-
-export async function loader() {
-    const projects = await getProjects();
-    const tags = await getTags();
-    return { projects, tags }
-}
-
-
-
-function SideBar() {
-    const { projects, tags } = useLoaderData();
-    return (
-        <div id="sidebar">
-            {/* Title */}
-            <NavLink to="/">
-                <h1>Menu</h1>
-            </NavLink>
-
-            {/* Search Form */}
-            <Form id="search-form" role="search">
-                <input
-                    type="search"
-                    name="q"
-                    placeholder="Find Project"
-                />
-                <div
-                    id="search-spinner"
-                    aria-hidden
-                    hidden={true}
-                />
-            </Form>
-
-            {/* Tasks */}
-            <h2>Tasks</h2>
-            <nav className="task_menu">
-                <ul>
-                    <NavLink className="upcoming" to="task/upcoming">Upcoming</NavLink>
-                    <NavLink className="today" to="task/today">Today</NavLink>
-                    <NavLink className="recent" to="task/most_recent">Recent</NavLink>
-                    <NavLink className="incomplete" to="task/?status=false">Incomplete</NavLink>
-                </ul>
-            </nav>
-
-            {/* Project */}
-            <h2>Projects</h2>
-            <nav className="project_menu">
-                {projects.length &&
-                    <ul>
-                        {projects.map((project) => (
-                            <NavLink key={project.id} to={`project/${project.id}`}>{project.title}</NavLink>
-                        ))}
-                    </ul>
-                }
-            </nav>
-
-            {/* Tag */}
-            <h2>Tags</h2>
-            <nav className="tag_menu">
-                {tags.length &&
-                    <ul>
-                        {tags.map((tag) => (
-                            <NavLink key={tag.id} to={`tag/${tag.id}`}>{tag.title}</NavLink>
-                        ))}
-                    </ul>
-                }
-            </nav>
-        </div>
-    );
-}
-
-
-
+// import { NavLink, Form, useLoaderData } from "react-router-dom";
+import { SideBar } from "./sidebar";
 
 export default function Root() {
-    return (
-        <>
-            <SideBar />
-            <Outlet />
-        </>
-    )
+  return (
+    <>
+      <SideBar />
+      <Outlet />
+    </>
+  );
 }
---
+++ frontend/src/routes/sidebar.jsx
@@ -0,0 +1,70 @@
+import { useLoaderData, NavLink, Form } from "react-router-dom";
+
+function Button({clickHandler}){
+  return (
+    <button 
+      className="new-item-button"
+      onClick={clickHandler}
+    >+</button>
+  )
+}
+
+export function SideBar() {
+    const projects = useLoaderData();
+    return (
+      <div id="sidebar">
+        {/* Title */}
+        <NavLink to="/">
+          <h1>Menu</h1>
+        </NavLink>
+  
+        {/* Search Form */}
+        <Form id="search-form" role="search">
+          <input type="search" name="q" placeholder="Find Project" />
+          <div id="search-spinner" aria-hidden hidden={true} />
+        </Form>
+  
+        {/* Tasks */}
+        <span className="flexSpan">
+          <h2>Tasks</h2>
+          <Button />
+        </span>
+        <nav className="task_menu">
+          <ul>
+            <NavLink className="upcoming" to="task/upcoming">
+              Upcoming
+            </NavLink>
+            <NavLink className="today" to="task/today">
+              Today
+            </NavLink>
+            <NavLink className="recent" to="task/most_recent">
+              Recent
+            </NavLink>
+            <NavLink className="incomplete" to="task/?status=false">
+              Incomplete
+            </NavLink>
+          </ul>
+        </nav>
+  
+        {/* Project */}
+        <span className="flexSpan">
+          <h2>Projects</h2>
+          <Button />
+        </span>
+        
+        <nav className="project_menu">
+          {projects.length && (
+            <ul>
+              {projects.map((project) => (
+                <NavLink key={project.id} to={`project/${project.id}`}>
+                  {project.title}
+                </NavLink>
+              ))}
+            </ul>
+          )}
+        </nav>
+        
+      </div>
+    );
+  }
+  
